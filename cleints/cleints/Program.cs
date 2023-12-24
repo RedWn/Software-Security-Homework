2a412e21d3cd865827f978e14044309e2192a7a7
RedWn <redwaniscool@gmail.com>
2023-12-24 20:10:38 +0300

    Update CLI

--- cleints/cleints/Program.cs
+++ cleints/cleints/Program.cs
@@ -6,10 +6,10 @@ namespace Clients
         static void Main(string[] args)
         {
             Console.WriteLine("let's connect to Multi-Threaded TCP Server");
-            Console.WriteLine("Provide us with your  IP:");
+            Console.WriteLine("Enter server IP:");
             String ip = Console.ReadLine();
 
-            Console.WriteLine("Provide Port:");
+            Console.WriteLine("Enter Port:");
             int port = Int32.Parse(Console.ReadLine());
 
             Client client = new Client(ip, port);
--- server/server/Program.cs
+++ server/server/Program.cs
@@ -7,7 +7,12 @@ namespace Multi_Threaded_TCP
         static void Main(string[] args)
         {
             Console.WriteLine("Multi-Threaded Server");
-            TcpServer server = new TcpServer(25);
+            Console.WriteLine("Enter server IP:");
+            String ip = Console.ReadLine();
+
+            Console.WriteLine("Enter Port:");
+            int port = Int32.Parse(Console.ReadLine());
+            TcpServer server = new TcpServer(ip,port);
         }
     }
 }
