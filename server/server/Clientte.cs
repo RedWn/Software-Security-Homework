0d7120c4b9c64c3a08048df88760c8576123920a
RedWn <redwaniscool@gmail.com>
2023-12-24 22:46:50 +0300

    Create Clientte.cs

---
+++ server/server/Clientte.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    internal class Clientte
+    {
+        public TcpClient client;
+        public IPEndPoint IPEndPoint;
+        public StreamWriter sWriter;
+        public StreamReader sReader;
+
+        public Clientte(object obj)
+        {
+            client = (TcpClient)obj;
+            IPEndPoint = client.Client.RemoteEndPoint as IPEndPoint;
+            sWriter = new StreamWriter(client.GetStream(), Encoding.ASCII);
+            sReader = new StreamReader(client.GetStream(), Encoding.ASCII);
+        }
+    }
+}
