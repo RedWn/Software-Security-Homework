62b488a153f96a4abdc11f095afdf6385e2dbff1
RedWn <redwaniscool@gmail.com>
2023-12-25 22:35:43 +0300

    Add Package Class

---
+++ server/server/Package.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    internal class Package
+    {
+        //SERVER
+        public string encryption;
+        public string type;
+
+        public Dictionary<string, string>? body;
+
+        public Package(string encryption, string type)
+        {
+            this.encryption = encryption;
+            this.type = type;
+        }
+    }
+}
